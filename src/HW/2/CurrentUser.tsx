d7f03c60383603809e031775dc7399a8b3a790a0
vaxich <vaxich@rambler.ru>
2023-11-02 11:03:14 +0300

    done 2 lesson

--- src/HW/2/CurrentUser.tsx
+++ src/HW/2/CurrentUser.tsx
@@ -1,10 +1,12 @@
 import { UserType } from './HW2';
 
 type CurrentUserPropsType = {
-  user: any // пропиши типизацию
+  user: UserType 
 };
 
 export const CurrentUser = (props: CurrentUserPropsType) => {
+
+ 
   return (
     <li key={props.user.id} id={`hw02-user-${props.user.id}`}>
       <strong>{props.user.name}</strong> (Age: {props.user.age})<strong> Address: </strong>
